71fa3dc7619d69719357b297c429d68a961739a3
Dan Sheerman <dan.sheerman@headscape.co.uk>
2020-05-05 15:54:11 +0100

    Typography

--- fractal.js
+++ fractal.js
@@ -42,13 +42,13 @@ fractal.components.set('statuses', {
 
 // Set some placeholder copy
 fractal.components.set('default.context', {
-  'placeholderHeadline': 'Fighting poverty through volunteering',
-  'placeholderHeadline2': 'From anger to action: How to stop 200,000 needless maternal deaths',
-  'placeholderHeadline3': 'The deaf volunteer struggling for health equality',
-  'placeholderHeadline4': 'Getting positive about HIV: busting myths with music',
-  'placeholderPara1': 'At VSO, we believe progress is only possible when we work together. Partnerships lie at the root of all the positive change we create.',
-  'placeholderPara2': 'Over the last 60 years VSO has worked in over 90 countries with more than 80,000 volunteers, and supported 50,000,000 people.',
-  'placeholderPara3': 'Hundreds of VSO volunteers and supporters contact their representatives about international development issues. For example, in the build-up to Parliamentary debates on the 0.7% foreign aid expenditure target. They also strengthened VSO’s Women in Power campaign, fighting for women\'s empowerment.',
+  'placeholderHeadline': 'Graduates give Staffs Uni star role in popular app',
+  'placeholderHeadline2': 'Enjoying the View? How computer games can help evaluate landscapes',
+  'placeholderHeadline3': '“It\'s Beautiful to be Old” Professor tells Vatican',
+  'placeholderHeadline4': 'Research into poverty in Stoke-on-Trent gets new funding boost',
+  'placeholderPara1': 'The founders of an immersive storytelling app have returned to their university stomping ground to film their latest episode.',
+  'placeholderPara2': 'Staffordshire University is going global with a programme packed full of free events to start the new decade.',
+  'placeholderPara3': '2020 marks the International Year of the Nurse and Midwife, and Staffordshire University is joining colleagues across the globe to shine a spotlight on the sector. Celebrations kick off with a talk this week by Visiting Professor Ann-Marie Cannaby, Chief Nurse at the Royal Wolverhampton Hospital, at the University’s Centre of Excellence in Healthcare Education, Stafford.Two hundred years may have passed since the birth of Florence Nightingale, but patient care and safety remain the most important influencing factors within the fields of nursing and midwifery. Professor Cannaby will use her keynote presentation to explore curriculum developments.',
   'lorem': 'Lipsum has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
   'loremLong': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
   'loremShort': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
