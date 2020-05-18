31bd9b6f9614037f850a9e5c1c508e42c1e766c9
Dan Sheerman <dan.sheerman@headscape.co.uk>
2020-05-18 16:49:28 +0100

    Start on megaNav

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,31 @@
+/* global jQuery */
+
 // import './vendor/waypoints/lib/jquery.waypoints';
 // import './vendor/slick-1.8.1/slick/slick';
 
+// import './../../components/megaNav/megaNav';
+
 (function ($) {
 
+  let megaNavInit = function (){
+    let megaNav = $('#megaNav');
+    let controls = megaNav.find('.megaNav__controls');
 
+    $('.has-children', megaNav).on('click', function (e) {
+      e.preventDefault();
+      $(this).parent().addClass('is-expanded');
+    });
+
+    $('.megaNav__closeLevel', megaNav).on('click', function (e) {
+      e.preventDefault();
+      $(this).closest('.is-expanded').removeClass('is-expanded');
+    });
+  };
+
+  // --
 
   $(document).ready(function () {
+    megaNavInit();
   });
 
 })(jQuery);
