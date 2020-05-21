0e18174c007a4e173a0a09bcc2061717aad1a691
Dan Sheerman <dan.sheerman@headscape.co.uk>
2020-05-21 16:45:54 +0100

    Build: Some nav bits, some IE fallbacks, some docs - many things

--- design-system/js/app.js
+++ design-system/js/app.js
@@ -1,18 +1,41 @@
 /* global jQuery */
 
-// import './vendor/waypoints/lib/jquery.waypoints';
-// import './vendor/slick-1.8.1/slick/slick';
+// @codekit-prepend quiet './vendor/jquery.hoverIntent';
+// @codekit-prepend quiet './vendor/smartResize';
 
-// import './../../components/megaNav/megaNav';
 
 (function ($) {
 
   let megaNavInit = function (){
     let megaNav = $('#megaNav');
-    let controls = megaNav.find('.megaNav__controls');
+    let megaNavFullBreakpoint = window.matchMedia('(min-width: 70rem)');
+
+    function updateMegaNavBreakpointClass(){
+      if (megaNavFullBreakpoint.matches) {
+        megaNav.addClass('is-full-breakpoint');
+      } else {
+        megaNav.removeClass('is-full-breakpoint');
+      }
+    }
+
+    updateMegaNavBreakpointClass();
+
+    $(window).smartResize(function(){
+      megaNavFullBreakpoint = window.matchMedia('(min-width: 70rem)');
+      updateMegaNavBreakpointClass();
+    });
+
+    // let controls = megaNav.find('.megaNav__controls');
 
     $('.has-children', megaNav).on('click', function (e) {
       e.preventDefault();
+      let item = $(this).parent();
+      let siblings = item.siblings();
+
+
+      $(siblings).find('.is-expanded').removeClass('is-expanded');
+      $(siblings).removeClass('is-expanded')
+
       $(this).parent().addClass('is-expanded');
     });
 
