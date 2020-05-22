b6b0efa70dd1596c7c1f48938bbeed9a6f8cc8f1
Dan Sheerman <dan.sheerman@headscape.co.uk>
2020-05-22 11:28:56 +0100

    megaNav\!

--- design-system/js/app.js
+++ design-system/js/app.js
@@ -6,37 +6,83 @@
 
 (function ($) {
 
-  let megaNavInit = function (){
+  let megaNavInit = function () {
     let megaNav = $('#megaNav');
     let megaNavFullBreakpoint = window.matchMedia('(min-width: 70rem)');
 
-    function updateMegaNavBreakpointClass(){
+    function updateMegaNavBreakpointClass() {
       if (megaNavFullBreakpoint.matches) {
-        megaNav.addClass('is-full-breakpoint');
-      } else {
-        megaNav.removeClass('is-full-breakpoint');
+        megaNav.addClass('is-largescreen');
+        megaNav.removeClass('is-smallscreen');
+      }
+      else {
+        megaNav.removeClass('is-largescreen');
+        megaNav.addClass('is-smallscreen');
       }
     }
 
     updateMegaNavBreakpointClass();
 
-    $(window).smartResize(function(){
+    $(window).smartResize(function () {
       megaNavFullBreakpoint = window.matchMedia('(min-width: 70rem)');
       updateMegaNavBreakpointClass();
     });
 
-    // let controls = megaNav.find('.megaNav__controls');
-
-    $('.has-children', megaNav).on('click', function (e) {
-      e.preventDefault();
-      let item = $(this).parent();
+    function expandChildren(link) {
+      let item = $(link).parent();
       let siblings = item.siblings();
-
 
       $(siblings).find('.is-expanded').removeClass('is-expanded');
       $(siblings).removeClass('is-expanded')
 
-      $(this).parent().addClass('is-expanded');
+      $(link).parent().addClass('is-expanded');
+    }
+
+    $(document).on('click', '#megaNav.is-smallscreen .has-children', function (e) {
+      e.preventDefault();
+      expandChildren($(this));
+    });
+
+    $(document).on('click', '#megaNav.is-largescreen .has-children', function (e) {
+      e.preventDefault();
+      expandChildren($(this));
+    });
+
+    $('#megaNav.is-largescreen').hoverIntent({
+      over: function () {
+        expandChildren($(this));
+        let item = $(this).parent();
+        let secondLevelLink = $('.megaNav__secondLevel-link.has-children', item).eq(0);
+        expandChildren(secondLevelLink);
+      },
+      out: function () {
+        // $(this).closest('.is-expanded').removeClass('is-expanded');
+      },
+      selector: '.megaNav__topLevel-link.has-children',
+      sensitivity: 25
+    });
+
+    $('#megaNav.is-largescreen').hoverIntent({
+      over: function () {
+        expandChildren($(this));
+        let item = $(this).parent();
+        let thirdLevelLink = $('.megaNav__thirdLevel-link.has-children', item).eq(0);
+        expandChildren(thirdLevelLink);
+      },
+      out: function () {
+        // $(this).closest('.is-expanded').removeClass('is-expanded');
+      },
+      selector: '.megaNav__secondLevel-link.has-children',
+      sensitivity: 75
+    });
+
+    $('#megaNav.is-largescreen').hoverIntent({
+      over: function () {
+      },
+      out: function () {
+        $('.is-expanded', megaNav).removeClass('is-expanded');
+      },
+      timeout: 250
     });
 
     $('.megaNav__closeLevel', megaNav).on('click', function (e) {
