54914ca100c1f6f58428d600e6962c4ca3fd2ad9
Dan Sheerman <dan.sheerman@headscape.co.uk>
2020-07-02 23:18:12 +0100

    Base typography, mobile spacing

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -47,10 +47,62 @@
       if (megaNavFullBreakpoint.matches) {
         megaNav.addClass('is-largescreen');
         megaNav.removeClass('is-smallscreen');
+
+        $('#megaNav.is-largescreen').hoverIntent({
+          over: function () {
+            expandChildren($(this));
+            let item = $(this).parent();
+            let secondLevelLink = $('.megaNav__secondLevel-link.has-children', item).eq(0);
+            expandChildren(secondLevelLink);
+          },
+          out: function () {
+            // $(this).closest('.is-expanded').removeClass('is-expanded');
+          },
+          selector: '.megaNav__topLevel-link.has-children',
+          sensitivity: 25
+        });
+
+        $('#megaNav.is-largescreen').hoverIntent({
+          over: function () {
+            expandChildren($(this));
+            let item = $(this).parent();
+            let thirdLevelLink = $('.megaNav__thirdLevel-link.has-children', item).eq(0);
+            expandChildren(thirdLevelLink);
+          },
+          out: function () {
+            // $(this).closest('.is-expanded').removeClass('is-expanded');
+          },
+          selector: '.megaNav__secondLevel-link.has-children',
+          sensitivity: 75
+        });
+
+        $('#megaNav.is-largescreen').hoverIntent({
+          over: function () {
+          },
+          out: function () {
+            $('.is-expanded', megaNav).removeClass('is-expanded');
+          },
+          timeout: 250
+        });
+
+        $('.megaNav__closeLevel', megaNav).on('click', function (e) {
+          e.preventDefault();
+          $(this).closest('.is-expanded').removeClass('is-expanded');
+        });
+
+        $(document).on('click', '#megaNav.is-largescreen .has-children', function (e) {
+          e.preventDefault();
+          expandChildren($(this));
+        });
       }
       else {
         megaNav.removeClass('is-largescreen');
         megaNav.addClass('is-smallscreen');
+
+        $(document).on('click', '#megaNav.is-smallscreen .has-children', function (e) {
+          e.preventDefault();
+          expandChildren($(this));
+        });
       }
     }
 
@@ -71,57 +123,6 @@
       $(link).parent().addClass('is-expanded');
     }
 
-    $(document).on('click', '#megaNav.is-smallscreen .has-children', function (e) {
-      e.preventDefault();
-      expandChildren($(this));
-    });
-
-    $(document).on('click', '#megaNav.is-largescreen .has-children', function (e) {
-      e.preventDefault();
-      expandChildren($(this));
-    });
-
-    $('#megaNav.is-largescreen').hoverIntent({
-      over: function () {
-        expandChildren($(this));
-        let item = $(this).parent();
-        let secondLevelLink = $('.megaNav__secondLevel-link.has-children', item).eq(0);
-        expandChildren(secondLevelLink);
-      },
-      out: function () {
-        // $(this).closest('.is-expanded').removeClass('is-expanded');
-      },
-      selector: '.megaNav__topLevel-link.has-children',
-      sensitivity: 25
-    });
-
-    $('#megaNav.is-largescreen').hoverIntent({
-      over: function () {
-        expandChildren($(this));
-        let item = $(this).parent();
-        let thirdLevelLink = $('.megaNav__thirdLevel-link.has-children', item).eq(0);
-        expandChildren(thirdLevelLink);
-      },
-      out: function () {
-        // $(this).closest('.is-expanded').removeClass('is-expanded');
-      },
-      selector: '.megaNav__secondLevel-link.has-children',
-      sensitivity: 75
-    });
-
-    $('#megaNav.is-largescreen').hoverIntent({
-      over: function () {
-      },
-      out: function () {
-        $('.is-expanded', megaNav).removeClass('is-expanded');
-      },
-      timeout: 250
-    });
-
-    $('.megaNav__closeLevel', megaNav).on('click', function (e) {
-      e.preventDefault();
-      $(this).closest('.is-expanded').removeClass('is-expanded');
-    });
 
 
     // @TODO: Check keyboard focus - could it be handled better?
