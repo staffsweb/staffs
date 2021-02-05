27f73f82540138163a62fdd6bbf92cbb39dcf3d7
Chris Goostry <c.goostry@staffs.ac.uk>
2021-02-05 10:35:29 +0000

    Lead generation - new scroll functionality

--- src/assets/dev/js/app.js
+++ src/assets/dev/js/app.js
@@ -6337,22 +6337,22 @@ var crmFormsInit = function crmFormsInit() {
 };
 
 var leadGenInit = function leadGenInit() {
-  var leadGenActive = $("#lead-gen").length > 0 ? true : false; // CG: Only if lead gen is present on the page
+  var leadGenActive = $("#lead-gen").length > 0 ? true : false; // CG: Only if lead gen is present on the page. In the back end, it is not written to the page is the cookie "HideLeadGen" is present.
+
+  if (leadGenActive) {
+    // CG: Hide the content we don't want the user to scroll past
+    $("#hide-for-lead-gen, #accolade-slider, #footer-site").addClass("visually-hidden");
+  }
 
   $(document).on("scroll", function (e) {
-    // CG: Limit scrolling past entry requirements
-    if (leadGenActive) {
-      var dontScrollPast = $("#entry").offset().top;
-      var windowScrollTop = $(document).scrollTop();
+    // CG: Activate the lead gen banner when the user reaches the top of the "hidden" content
+    var scrollBottom = $(window).scrollTop() + $(window).height();
+    var targetDivPos = $("#hide-for-lead-gen").position().top - 100;
 
-      if (windowScrollTop >= dontScrollPast) {
-        $("#lead-gen").slideDown("slow");
-        $(document).scrollTop(dontScrollPast);
-      }
-
-      if (windowScrollTop < dontScrollPast - 10) {
-        $("#lead-gen").slideUp("slow");
-      }
+    if (scrollBottom >= targetDivPos && leadGenActive) {
+      $("#lead-gen").slideDown("slow");
+    } else {
+      $("#lead-gen").slideUp("slow");
     }
   });
   $("#lead-gen__submit-btn").on("click", function (e) {
@@ -6385,7 +6385,6 @@ var leadGenInit = function leadGenInit() {
         $("#loading-spinner--lead-gen").hide(); // CG: Replace the form fields with the success message
 
         $("#lead-gen__form").html("<h2>Thank you!</h2><p class='font-3xl'>Your request has been sent.</p>");
-        $('.slick-slider').slick('refresh');
         setCookie("HideLeadGen", 1, 30);
         setTimeout(function () {
           $("#lead-gen-modal .modal__close").trigger("click");
@@ -6403,6 +6402,8 @@ var leadGenInit = function leadGenInit() {
     $(".lead-gen").slideUp("slow");
     setCookie("HideLeadGen", 1, 30);
     leadGenActive = false;
+    $("#hide-for-lead-gen, #accolade-slider, #footer-site").removeClass("visually-hidden");
+    $('.slick-slider').slick('refresh');
   });
 }; // @TODO: at some point, it'd probably be nice if functions sat in
 // 'eachIndividualComponentName.js' in each component folder and were imported
